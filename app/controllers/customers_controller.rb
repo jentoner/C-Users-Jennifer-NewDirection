c81769aa603bc993d160387c56f977412490c6f4
Jennifer Toner <wilhelmena3@gmail.com>
2016-11-27 03:28:04 -0600

    Set the Customer layout. Customer page is good to go

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -2,6 +2,6 @@
 class CustomersController < ApplicationController
   # Rails automatically laods the app/views/customers/inex.html.erb
   def index
-    @customers = Customer.order('fullname').page(params[:page]).per(5)
+    @customers = Customer.order('fullname').page(params[:page]).per(3)
   end
 end
